1c8e77dcb7b6eb11807352189d06b01ea4df7ba9
Matt Blanchette <mblanchette@usm.maine.edu>
2009-10-09 19:49:39 +0000

    Added mobile and server WS interfaces using shared WS types

---
+++ src/main/java/org/motechproject/ws/Patient.java
@@ -0,0 +1,41 @@
+package org.motechproject.ws;
+
+/**
+ * A simple Patient data transfer object.
+ *
+ * @author Kofi A. Asamoah (yoofi@dreamoval.com)
+ * Date Created Jul 31, 2009
+ *
+ */
+public class Patient {
+    private String name;
+    private String serialNumber;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the serialNumber
+     */
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    /**
+     * @param serialNumber the serialNumber to set
+     */
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}
