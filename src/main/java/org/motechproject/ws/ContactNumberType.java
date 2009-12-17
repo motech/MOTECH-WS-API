a348a98b0ee7700318a5faa860b6e0243ba1ede6
Matt Blanchette <mblanchette@usm.maine.edu>
2009-12-17 18:22:45 +0000

    Updated contact number type to match spec

--- src/main/java/org/motechproject/ws/ContactNumberType.java
+++ src/main/java/org/motechproject/ws/ContactNumberType.java
@@ -3,9 +3,8 @@ package org.motechproject.ws;
 /**
  * An enumeration of contact number types
  * 
- * @author Kofi A. Asamoah (yoofi@dreamoval.com) 
- * Date Created: Jul 31, 2009
+ * @author Kofi A. Asamoah (yoofi@dreamoval.com) Date Created: Jul 31, 2009
  */
 public enum ContactNumberType {
-	PERSONAL, SHARED
+	PERSONAL, HOUSEHOLD, PUBLIC
 }
