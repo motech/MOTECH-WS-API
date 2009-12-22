1894ddc83c0d0dd9ed2542edc22c9b8ee969d367
Matt Blanchette <mblanchette@usm.maine.edu>
2009-12-22 21:18:27 +0000

    Reduced server interface to only methods in spec

--- src/main/java/org/motechproject/ws/server/RegistrarService.java
+++ src/main/java/org/motechproject/ws/server/RegistrarService.java
@@ -6,11 +6,8 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
-import org.motechproject.ws.ContactNumberType;
-import org.motechproject.ws.DeliveryTime;
 import org.motechproject.ws.Gender;
 import org.motechproject.ws.LogType;
-import org.motechproject.ws.MediaType;
 
 /**
  * The service endpoint interface for the major motech server web service
@@ -19,47 +16,6 @@ import org.motechproject.ws.MediaType;
  */
 @WebService
 public interface RegistrarService {
-
-	@WebMethod
-	public void registerPatient(
-			@WebParam(name = "nursePhoneNumber") String nursePhoneNumber,
-			@WebParam(name = "serialId") String serialId,
-			@WebParam(name = "name") String name,
-			@WebParam(name = "community") String community,
-			@WebParam(name = "location") String location,
-			@WebParam(name = "dateOfBirth") Date dateOfBirth,
-			@WebParam(name = "gender") Gender gender,
-			@WebParam(name = "nhis") Integer nhis,
-			@WebParam(name = "phoneNumber") String phoneNumber,
-			@WebParam(name = "contactNumberType") ContactNumberType contactNumberType,
-			@WebParam(name = "language") String language,
-			@WebParam(name = "mediaType") MediaType mediaType,
-			@WebParam(name = "deliveryTime") DeliveryTime deliveryTime,
-			@WebParam(name = "messagePrograms") String[] messagePrograms);
-
-	@WebMethod
-	public void recordMaternalVisit(
-			@WebParam(name = "nursePhoneNumber") String nursePhoneNumber,
-			@WebParam(name = "date") Date date,
-			@WebParam(name = "serialId") String serialId,
-			@WebParam(name = "tetanus") Boolean tetanus,
-			@WebParam(name = "ipt") Boolean ipt,
-			@WebParam(name = "itn") Boolean itn,
-			@WebParam(name = "visitNumber") Integer visitNumber,
-			@WebParam(name = "onARV") Boolean onARV,
-			@WebParam(name = "prePMTCT") Boolean prePMTCT,
-			@WebParam(name = "testPMTCT") Boolean testPMTCT,
-			@WebParam(name = "postPMTCT") Boolean postPMTCT,
-			@WebParam(name = "hemoglobinAt36Weeks") Double hemoglobinAt36Weeks);
-
-	@WebMethod
-	public void registerPregnancy(
-			@WebParam(name = "nursePhoneNumber") String nursePhoneNumber,
-			@WebParam(name = "date") Date date,
-			@WebParam(name = "serialId") String serialId,
-			@WebParam(name = "dueDate") Date dueDate,
-			@WebParam(name = "parity") Integer parity,
-			@WebParam(name = "hemoglobin") Double hemoglobin);
 
 	@WebMethod
 	public void recordGeneralVisit(
