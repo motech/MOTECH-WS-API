5066c7a1f054aec0c54906ff0df4cc4b20a82eac
Matt Blanchette <mblanchette@usm.maine.edu>
2010-02-18 18:33:31 +0000

    Added Care and updated Patient to support queries

--- src/main/java/org/motechproject/ws/Patient.java
+++ src/main/java/org/motechproject/ws/Patient.java
@@ -1,43 +1,116 @@
 package org.motechproject.ws;
 
-import java.io.Serializable;
+import java.util.Date;
 
-/**
- * A simple Patient data transfer object.
- *
- * @author Kofi A. Asamoah (yoofi@dreamoval.com)
- * Date Created Jul 31, 2009
- *
- */
-public class Patient implements Serializable{
-    private String name;
-    private String serialNumber;
+public class Patient {
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+	String motechId;
+	String preferredName;
+	String firstName;
+	String lastName;
+	Date birthDate;
+	Integer age;
+	Gender sex;
+	String community;
+	String phoneNumber;
+	Date estimateDueDate;
+	Date deliveryDate;
+	Care[] cares;
 
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getMotechId() {
+		return motechId;
+	}
 
-    /**
-     * @return the serialNumber
-     */
-    public String getSerialNumber() {
-        return serialNumber;
-    }
+	public void setMotechId(String motechId) {
+		this.motechId = motechId;
+	}
 
-    /**
-     * @param serialNumber the serialNumber to set
-     */
-    public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber;
-    }
+	public String getPreferredName() {
+		return preferredName;
+	}
+
+	public void setPreferredName(String preferredName) {
+		this.preferredName = preferredName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Gender getSex() {
+		return sex;
+	}
+
+	public void setSex(Gender sex) {
+		this.sex = sex;
+	}
+
+	public String getCommunity() {
+		return community;
+	}
+
+	public void setCommunity(String community) {
+		this.community = community;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getEstimateDueDate() {
+		return estimateDueDate;
+	}
+
+	public void setEstimateDueDate(Date estimateDueDate) {
+		this.estimateDueDate = estimateDueDate;
+	}
+
+	public Date getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(Date deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public Care[] getCares() {
+		return cares;
+	}
+
+	public void setCares(Care[] cares) {
+		this.cares = cares;
+	}
+
 }
